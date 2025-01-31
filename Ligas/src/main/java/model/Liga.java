64b51615f0684567eac3e5c0dc150138f93bb381
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2025-01-31 18:42:38 +0100

    Clase Liga hecha

--- Ligas/src/main/java/model/Liga.java
+++ Ligas/src/main/java/model/Liga.java
@@ -1,4 +1,68 @@
 package model;
 
-public class Liga {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "ligas")
+public class Liga implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nombre_liga")
+    private String nombreLiga;
+
+    @Column(name = "fecha_inicio")
+    private Date fechaInicio;
+
+    @Column(name = "fecha_fin")
+    private Date fechaFin;
+
+    @OneToMany(mappedBy = "liga", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Equipo> equipos;
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreLiga() {
+        return nombreLiga;
+    }
+
+    public void setNombreLiga(String nombreLiga) {
+        this.nombreLiga = nombreLiga;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public List<Equipo> getEquipos() {
+        return equipos;
+    }
+
+    public void setEquipos(List<Equipo> equipos) {
+        this.equipos = equipos;
+    }
 }
